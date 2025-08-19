d582d33472b26d2b4dffab72fa25fbe977de363b
BXRSRUDIOS <bilalrajastrikes23@gmail.com>
2025-08-19 22:12:57 +0100

    Topic 2.5 Populated + started testing user interfaces

---
+++ ui files/testUi.py
@@ -0,0 +1,26 @@
+import PyQt6
+
+# From homePage.ui import the ui file and make it viewable
+from PyQt6 import uic
+from PyQt6.QtWidgets import QMainWindow, QApplication
+import sys
+import os
+from dotenv import load_dotenv
+# Load environment variables from the .env file
+load_dotenv()
+# Set the path to the UI file
+ui_file_path = os.path.join(os.path.dirname(__file__), 'homePage.ui')
+# Load the UI file
+class HomePage(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi(ui_file_path, self)
+        self.setWindowTitle("Home Page")  # Set the window title
+        self.setGeometry(100, 100, 800, 800)
+def main():
+    app = QApplication(sys.argv)
+    window = HomePage()
+    window.show()
+    sys.exit(app.exec())
+if __name__ == "__main__":
+    main()

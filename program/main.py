23fc3c5382eec65c0c5c79e9cd93f9b584d737b6
BXRSRUDIOS <bilalrajastrikes23@gmail.com>
2025-08-29 19:39:36 +0100

    Started to setup the User Class by adding its definition

--- program/main.py
+++ program/main.py
@@ -1,5 +1,9 @@
 # Import relevant modules
 import sys
+import psycopg2
+import os
+import io
+from dotenv import load_dotenv
 from PyQt6.QtWidgets import QApplication, QMainWindow, QStackedWidget
 from PyQt6 import uic
 from PyQt6.QtCore import QSize
@@ -21,6 +25,12 @@ from manageClassPage import ManageClass
 from manageHomeworkPage import ManageHomework
 from studentStatisticsPage import StudentStatistics
 from teacherStatisticsPage import TeacherStatistics
+
+# Environment variables from the .env file need loading
+load_dotenv()
+
+# Enforce UTF-8 encoding for the database connection
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 class Controller(QMainWindow):
     def __init__(self):
@@ -83,6 +93,8 @@ class Controller(QMainWindow):
         self.manage_homework.setController(self)
         self.student_statistics.setController(self)
         self.teacher_statistics.setController(self)
+
+        # Controllers for other classes
     
     def handlePageChange(self, nameForIndex):
         # This will change the index to the relevant page when called
@@ -108,11 +120,71 @@ class Controller(QMainWindow):
         if nameForIndex in pagesIndex:
             self.stackedWidget.setCurrentIndex(pagesIndex[nameForIndex])
     
+    def database(self, query=None, parameter=None, queryType=None):
+        # This function will handle query execution to the database.
+        # It must check if the query requires parameters or not (ie for inserting statements because %s will be needed in the query and will have different execution methods)
+
+        # Connect to the database using environment variables for security
+        try:
+            conn = psycopg2.connect(
+                user=os.getenv("DB_USER"),
+                password=os.getenv("POSTGRESQL_PASSWORD"),
+                host=os.getenv("DB_HOST"),
+                port=os.getenv("DB_PORT"),
+                database=os.getenv("DB_NAME")
+            )
+            
+            # Create cursor to execute the queries
+            cur = conn.cursor()
+            if parameter:
+                cur.execute(query, parameter)
+            else:
+                cur.execute(query)
+            
+            # Check query type to see if fetching or changing the database
+            if queryType == "fetchItems":
+                result = cur.fetchall()
+                return result
+            elif queryType == "changeDatabase":
+                conn.commit()
+                return cur.rowcount
+            
+            # Close connection & cursor
+            cur.close()
+            conn.close()
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return None
+
     def run(self):
         # Actually goes and runs the application by showing the stacked widget
         self.stackedWidget.show()
         self.show()
         self.handlePageChange("home") # Start on the home page
+
+class User:
+    def __init__(self, user_id, firstName, surname, username, email, password, accountType):
+        self.user_id = user_id
+        self.firstName = firstName
+        self.surname = surname
+        self.username = username
+        self.email = email
+        self.accountType = accountType
+
+        # Leave password and salt as None for now, when creating a user account or logging in, these will be set to the hashed password values
+        self.hashedPassword = None
+        self.salt = None
+
+        # Link to the controller so that database functions can be called. Set to None but will be changed in controller class
+        self.controller = None
+
+        # Other attributes can be added later on as needed
+        self.loggedIn = False
+
+    def setController(self, controller):
+        # Function to set the controller for the user class so that database functions can be called
+        self.controller = controller
 
 if __name__ == "__main__":
     # Load up the program
